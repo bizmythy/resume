8ab0f6587ac78e9c357a070bdb50dc3dbf947097
bizmythy <andrew.p.council@gmail.com>
2026-05-17 12:48:05 -0400

    add typst

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
             (aspellWithDicts (ps: with ps; [ en ]))
             # keep-sorted start
             nushell
+            typst
             # keep-sorted end
           ];
         };
